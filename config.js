8f3418aca47ccaa89b294574a3a00410ce298a53
sahan-mp4 <sahangithub@gmail.com>
2025-11-02 19:22:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~YZpinQQZ#qW-Na8i4Nitd1QdyGhlYc9k7Dye4AsOUQ13HHWs7DFY'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~AUBgwTLS#gzV3rw4HxzIDp55jYFjSvu7bmKBw4xg9F5bVrRLBdKA'
 };

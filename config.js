6886348e8d50f71b321b0afafa1c03016dd74093
sahan-mp4 <sahangithub@gmail.com>
2025-12-12 20:49:33 +0530

    Update SESSION_ID default value in config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?xqob0e'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?2cvGNj'
 };
